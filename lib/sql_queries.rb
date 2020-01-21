9e733962aa48c26c8c2f34d073e01c0975e2a062
Neil Ryan <neilmarkoliverryan@gmail.com>
2020-01-21 23:00:41 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,7 @@ end
 def selects_most_prominent_color_and_returns_with_count
   "SELECT MAX(color), COUNT(color) 
   FROM bears
-  GROUP BY color;"
+  GROUP BY color DESC;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
