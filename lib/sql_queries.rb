c1f4c791b6329e69f571e13703c218ac893166f1
Neil Ryan <neilmarkoliverryan@gmail.com>
2020-01-22 00:33:44 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,7 +27,9 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT count() 
+  WHERE 
+  "
 end
 
 def selects_bear_that_killed_Tim
